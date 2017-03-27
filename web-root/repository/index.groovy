b3f3cafe87392ef75751b6716fe500e876304e19
dlukyanov <dlukyanov@ukr.net>
2017-03-27 10:14:04 +0300

    support HEAD request limit to GET format file listing with doublequotes <a href="..">... so /repository  can be used by ivy as resolver-url

--- web-root/repository/index.groovy
+++ web-root/repository/index.groovy
@@ -5,33 +5,37 @@ try {
 	assert Props.require("repository.root")
 	File repoRoot = new File( Props."repository.root" )
 	def path = request.getPathInfo()
+	def method = request.getMethod()
 	
 	if(path){
 		File repoPath = new File(repoRoot, path)
 		if( repoPath.exists() ){
 			if(repoPath.isDirectory()){
 				if(path.endsWith("/")){
+					assert method in ['GET','HEAD']
 					def listFiles = repoPath.listFiles().sort{ a,b-> a.isFile() <=> b.isFile() ?: a.getName() <=> b.getName() }
 					response.setStatus(200)
 					response.setContentType("text/html")
-					
-					html.html{
-						head{
-							title("rest-ivy:${path}")
-						}
-						body{
-							h2("rest-ivy:${path}")
-							ul{
-								if( path!="/" ){
-									li{
-										a(href:"..", ".." )
+					if(method=='GET'){
+						html.setDoubleQuotes(true)
+						html.html{
+							head{
+								title("rest-ivy:${path}")
+							}
+							body{
+								h2("rest-ivy:${path}")
+								ul{
+									if( path!="/" ){
+										li{
+											a(href:"..", ".." )
+										}
 									}
-								}
-								
-								listFiles.each{ File file ->
-									li{
-										def name = file.getName() + (file.isDirectory() ?"/":"")
-										a( href:name, name )
+									
+									listFiles.each{ File file ->
+										li{
+											def name = file.getName() + (file.isDirectory() ?"/":"")
+											a( href:name, name )
+										}
 									}
 								}
 							}
@@ -42,6 +46,7 @@ try {
 					response.setHeader("Location", ".${path}/");
 				}
 			}else{
+				assert method in ['GET','HEAD']
 				response.setStatus(200)
 				response.setContentType( URLConnection.guessContentTypeFromName(repoPath.getName()) ?: "application/octet-stream" )
 				response.setContentLength( (int)repoPath.length() ) //TODO:int???
@@ -49,12 +54,13 @@ try {
 				response.setHeader("x-file-name", repoPath.getName());
 				response.setHeader("x-last-modified", new java.sql.Timestamp( repoPath.lastModified() ).toString());
 				response.setHeader("Last-Modified", Dates.httpFormat( repoPath.lastModified() ) );
-				
-				//write content into output stream
-				def stream = response.getOutputStream()
-				stream << repoPath.newInputStream()
-				stream.flush()
-				stream.close()
+				if(method=='GET'){
+					//write content into output stream
+					def stream = response.getOutputStream()
+					stream << repoPath.newInputStream()
+					stream.flush()
+					stream.close()
+				}
 			}
 			
 		}else{
